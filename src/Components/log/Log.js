239e8af64ac2108c198cd97e60a5819256f869ce
clocktower39 <clocktower39@aol.com>
2021-05-01 14:43:47 -0700

    styling

--- src/Components/log/Log.js
+++ src/Components/log/Log.js
@@ -7,17 +7,6 @@ export const Log = (props) => {
     const goals = useSelector(state => state.goals);
     return (
         <Grid container>
-        <Grid container item xs={12} variant={"h3"}>
-            <Grid item xs={4}>
-                <Typography variant={'h4'}>Task</Typography>
-            </Grid>
-            <Grid item xs={4}>
-                <Typography variant={'h4'}>Interval</Typography>
-            </Grid>
-            <Grid item xs={4}>
-                <Typography variant={'h4'}>Target</Typography>
-            </Grid>
-        </Grid>
             {goals.map((goal, index) => <LogItem goal={goal} achieved={goal.achieved} index={index} key={index} />)}
         </Grid>
     )
--- src/Components/log/LogItem.js
+++ src/Components/log/LogItem.js
@@ -1,45 +1,56 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { Box, CircularProgress, Grid, IconButton, Typography } from '@material-ui/core';
+import { AddCircle, RemoveCircle } from '@material-ui/icons';
 import { updateActivity } from '../../Redux/actions';
 
 export default function LogItem(props) {
-    const circularProgressPercent = Number(props.achieved/props.goal.targetPerDuration*100)>100?100:Number(props.achieved/props.goal.targetPerDuration*100);
+    let circularProgressPercent = Number(props.achieved/props.goal.targetPerDuration*100);
+    if(circularProgressPercent>100){
+        circularProgressPercent = 100;
+    }
+    else if(circularProgressPercent<0){
+        circularProgressPercent = 0;
+    }
     const dispatch = useDispatch();
     const handleActivityUpdate = (taskIndex, newAchieved) => {
         dispatch(updateActivity(taskIndex, newAchieved))
     }
     return (
       <Grid container item xs={12} variant={"h3"}>
-        <Grid item xs={4}>
-            {props.goal.task}
+        <Grid container item xs={4} alignItems={'center'}>
+            <Typography variant={'h5'} >{props.goal.task}</Typography>
         </Grid>
-        <Grid item xs={4}>
-            {props.goal.interval}
-        </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={8} container alignItems={'center'}>
+            <IconButton
+                onClick={()=>handleActivityUpdate(props.index, (props.goal.achieved - 1))}
+            >
+                <RemoveCircle/>
+            </IconButton>
+            <Box position="relative" display="inline-flex" alignItems={'center'}>
+                <CircularProgress variant="determinate" value={circularProgressPercent} size={100}/>
+                <Box
+                    top={0}
+                    left={0}
+                    bottom={0}
+                    right={0}
+                    position="absolute"
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                >
+                    <Typography variant="caption" component="div" color="textSecondary">
+                        {`${Math.round(Number(props.goal.achieved/props.goal.targetPerDuration*100))}%`}
+                    </Typography>
+                </Box>
+            </Box>
+            
             <IconButton
                 onClick={()=>handleActivityUpdate(props.index, (props.goal.achieved + 1))}
             >
-                <Box position="relative" display="inline-flex">
-                    <CircularProgress variant="determinate" value={circularProgressPercent} />
-                    <Box
-                        top={0}
-                        left={0}
-                        bottom={0}
-                        right={0}
-                        position="absolute"
-                        display="flex"
-                        alignItems="center"
-                        justifyContent="center"
-                    >
-                        <Typography variant="caption" component="div" color="textSecondary">
-                            {`${Math.round(Number(props.goal.achieved/props.goal.targetPerDuration*100))}%`}
-                        </Typography>
-                    </Box>
-                </Box>
+                <AddCircle/>
             </IconButton>
-            {props.goal.achieved}/{props.goal.targetPerDuration}
+            <Typography variant={'caption'}>{props.goal.achieved}/{props.goal.targetPerDuration}</Typography>
         </Grid>
       </Grid>
     );
