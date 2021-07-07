aa52ebf008b834310c33aa53186ba05da992424c
clocktower39 <clocktower39@aol.com>
2021-07-06 23:03:27 -0700

    add to dates without history

--- src/Redux/actions.js
+++ src/Redux/actions.js
@@ -5,12 +5,35 @@ export function updateActivityProgress(index, achieved, date) {
         const newState = { ...getState() };
         newState.goals.map((goal, i) => {
             if (i === index) {
+                (goal.history.some( day => day[date] === date))?
                 goal.history.map(day => {
                     if (day.date === date) {
                         day.achieved = day.achieved + achieved;
                     }
                     return day;
+                }):
+                goal.history.push({
+                    date,
+                    targetPerDuration: goal.defaultTarget,
+                    achieved,
                 })
+            }
+
+            return goal;
+        })
+        return dispatch({
+            type: UPDATE_ACTIVITY,
+            newState,
+        })
+    }
+}
+
+export function addDateToHistory(index, historyObject) {
+    return async (dispatch, getState) => {
+        const newState = { ...getState() };
+        newState.goals.map((goal, i) => {
+            if (i === index) {
+                goal.history.push({historyObject})
             }
 
             return goal;
