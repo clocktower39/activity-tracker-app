33bfc336736ab0a84fa898b65eb86a34975aadf2
clocktower39 <clocktower39@aol.com>
2021-07-11 08:46:30 -0700

    dark theme update for edit, reset TextFields on add

--- src/Components/Edit/EditGoal.js
+++ src/Components/Edit/EditGoal.js
@@ -1,9 +1,39 @@
 import React, { useState } from "react";
 import { useDispatch } from 'react-redux';
-import { Button, Grid, TextField, Typography } from "@material-ui/core";
+import { Button, Grid, TextField, Typography, makeStyles } from "@material-ui/core";
 import { EditActivity } from "../../Redux/actions";
 
+const useStyles = makeStyles({
+    Button:{
+        color: '#ffffff',
+    },
+    TextField: {
+      borderBottomColor: '#ccc',
+      "& input": {
+        color: "#ccc",
+      },
+      "& label": {
+        color: "#ccc",
+      },
+      "& label.Mui-focused": {
+        color: "#ccc",
+      },
+      "& .MuiOutlinedInput-root": {
+        "&.Mui-focused fieldset": {
+          borderColor: "#ccc",
+        },
+      },
+      "& .MuiInput-underline:before": {
+        borderBottomColor: "white",
+      },
+      "& .MuiInput-underline:after": {
+        borderBottomColor: "white",
+      },
+    },
+})
+
 export default function EditGoal(props) {
+    const classes = useStyles();
     const dispatch = useDispatch();
     const [editMode, setEditMode] = useState(false);
     const [task, setTask] = useState(props.goal.task);
@@ -23,18 +53,18 @@ export default function EditGoal(props) {
     }
 
     return editMode?(
-        <Grid container item xs={12}>
-            <Grid item xs={3} ><TextField value={task} onChange={(e)=>handleChange(e, setTask)} /></Grid>
-            <Grid item xs={3} ><TextField value={category} onChange={(e)=>handleChange(e, setCategory)} /></Grid>
-            <Grid item xs={3} ><TextField value={defaultTarget} onChange={(e)=>handleChange(e, setDefaultTarget)} /></Grid>
-            <Grid item xs={3} ><Button onClick={toggleEditMode}>Save</Button></Grid>
+        <Grid container item xs={12} className={classes.root}>
+            <Grid item xs={3} ><TextField className={classes.TextField} value={task} onChange={(e)=>handleChange(e, setTask)} /></Grid>
+            <Grid item xs={3} ><TextField className={classes.TextField} value={category} onChange={(e)=>handleChange(e, setCategory)} /></Grid>
+            <Grid item xs={3} ><TextField className={classes.TextField} value={defaultTarget} onChange={(e)=>handleChange(e, setDefaultTarget)} /></Grid>
+            <Grid item xs={3} ><Button className={classes.Button} onClick={toggleEditMode}>Save</Button></Grid>
         </Grid>
     ):(
-        <Grid container item xs={12}>
+        <Grid container item xs={12} className={classes.root}>
             <Grid item xs={3} ><Typography variant="body1">{props.goal.task}</Typography></Grid>
             <Grid item xs={3} ><Typography variant="body1">{props.goal.category}</Typography></Grid>
             <Grid item xs={3} ><Typography variant="body1">{props.goal.defaultTarget}</Typography></Grid>
-            <Grid item xs={3} ><Button onClick={toggleEditMode}>Edit</Button></Grid>
+            <Grid item xs={3} ><Button className={classes.Button} onClick={toggleEditMode}>Edit</Button></Grid>
         </Grid>
     )
 }
--- src/Components/Edit/EditLayout.js
+++ src/Components/Edit/EditLayout.js
@@ -1,11 +1,45 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Grid, IconButton, TextField, Typography } from "@material-ui/core";
+import { Grid, IconButton, TextField, Typography, makeStyles } from "@material-ui/core";
 import EditGoal from './EditGoal';
 import { AddCircle } from '@material-ui/icons';
 import { AddNewActivity } from "../../Redux/actions";
 
+const useStyles = makeStyles({
+    root: {
+        color: '#ffffff',
+    },
+    TableHead:{
+        paddingBottom: '12.5px',
+    },
+    TextField: {
+      borderBottomColor: '#ccc',
+      "& input": {
+        color: "#ccc",
+      },
+      "& label": {
+        color: "#ccc",
+      },
+      "& label.Mui-focused": {
+        color: "#ccc",
+      },
+      "& .MuiOutlinedInput-root": {
+        "&.Mui-focused fieldset": {
+          borderColor: "#ccc",
+        },
+      },
+      "& .MuiInput-underline:before": {
+        borderBottomColor: "white",
+      },
+      "& .MuiInput-underline:after": {
+        borderBottomColor: "white",
+      },
+    },
+
+});
+
 export default function EditLayout() {
+    const classes = useStyles();
     const dispatch = useDispatch();
     const goals = useSelector(state => state.goals);
     const [newTask, setNewTask] = useState('');
@@ -21,13 +55,17 @@ export default function EditLayout() {
             task: newTask,
             category: newCategory,
             defaultTarget: newTarget
-        }))
+        })).then(()=>{
+            setNewTask('');
+            setNewCategory('');
+            setNewTarget('');
+        })
     }
 
     return (
-        <div>
+        <div className={classes.root}>
             <Grid container >
-                <Grid container item xs={12}>
+                <Grid container item xs={12} className={classes.TableHead}>
                         <Grid item xs={3} ><Typography variant="h6" >Task</Typography></Grid>
                         <Grid item xs={3} ><Typography variant="h6" >Category</Typography></Grid>
                         <Grid item xs={3} ><Typography variant="h6" >Target</Typography></Grid>
@@ -36,10 +74,10 @@ export default function EditLayout() {
                 {goals.map((goal, index) => <EditGoal goal={goal} index={index} />)}
 
                 <Grid container item xs={12}>
-                    <Grid item xs={3} ><TextField label="Task" value={newTask} onChange={(e)=>handleNewEntryFields(e, setNewTask)} fullWidth/></Grid>
-                    <Grid item xs={3} ><TextField label="Category" value={newCategory} onChange={(e)=>handleNewEntryFields(e, setNewCategory)} fullWidth/></Grid>
-                    <Grid item xs={3} ><TextField label="Target" value={newTarget} onChange={(e)=>handleNewEntryFields(e, setNewTarget)} fullWidth/></Grid>
-                    <Grid item xs={3} ><IconButton onClick={handleAddClick}><AddCircle /></IconButton></Grid>
+                    <Grid item xs={3} ><TextField className={classes.TextField} label="Task" value={newTask} onChange={(e)=>handleNewEntryFields(e, setNewTask)} /></Grid>
+                    <Grid item xs={3} ><TextField className={classes.TextField} label="Category" value={newCategory} onChange={(e)=>handleNewEntryFields(e, setNewCategory)} /></Grid>
+                    <Grid item xs={3} ><TextField className={classes.TextField} label="Target" value={newTarget} onChange={(e)=>handleNewEntryFields(e, setNewTarget)} /></Grid>
+                    <Grid item xs={3} ><IconButton onClick={handleAddClick}><AddCircle style={{color: '#ffffff'}} /></IconButton></Grid>
                 </Grid>
             </Grid>
         </div>
