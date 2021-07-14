29fd5773e04afe737e3c9d03df93430c349b39f3
clocktower39 <clocktower39@aol.com>
2021-07-14 12:59:58 -0700

    fixed no history with new date bug

--- src/Components/log/Log.js
+++ src/Components/log/Log.js
@@ -45,7 +45,7 @@ const useStyles = makeStyles({
       borderBottomColor: "white",
     },
   },
-  ArrowButton:{
+  ArrowButton: {
     color: '#fff'
   },
 });
@@ -55,23 +55,35 @@ export const Log = () => {
   const goals = useSelector((state) => state.goals);
   const dateToISOLikeButLocal = (date) => {
     const offsetMs = date.getTimezoneOffset() * 60 * 1000;
-    const msLocal =  date.getTime() - offsetMs;
+    const msLocal = date.getTime() - offsetMs;
     const dateLocal = new Date(msLocal);
     const iso = dateLocal.toISOString();
     const isoLocal = iso.slice(0, 19);
     return isoLocal;
-}
+  }
 
-  const [selectedDate, setSelectedDate] = useState(dateToISOLikeButLocal(new Date()).substr(0,10));
+  const [selectedDate, setSelectedDate] = useState(dateToISOLikeButLocal(new Date()).substr(0, 10));
 
   const changeDate = (change) => {
     let newDate = new Date(selectedDate).setDate(new Date(selectedDate).getDate() + change);
-    setSelectedDate(new Date(newDate).toISOString().substr(0,10));
+    setSelectedDate(new Date(newDate).toISOString().substr(0, 10));
 
   }
-  
-  let allGoalsHistory = goals.map(goal=>{ return {history: goal.history, category: goal.category}});
-  let allGoalsStatsToday = allGoalsHistory.map(goal => { return { stats:goal.history.filter(day => day.date === selectedDate)[0], category: goal.category, defaultTarget: goal.defaultTarget}})
+
+  let allGoalsHistory = goals.map(goal => { return { history: goal.history, category: goal.category,  defaultTarget: goal.defaultTarget} });
+  let allGoalsStatsToday = allGoalsHistory.map(goal => {
+    return { 
+      stats: goal.history.filter(day => day.date === selectedDate)[0]
+      ?goal.history.filter(day => day.date === selectedDate)[0]
+      :{
+        date: selectedDate,
+        targetPerDuration: goal.defaultTarget,
+        achieved: 0,
+      },
+      category: goal.category,
+      defaultTarget: goal.defaultTarget 
+    }
+  })
 
   let categories = [];
 
@@ -87,24 +99,17 @@ export const Log = () => {
     let goalTotal = 0;
 
     categoryHistory.forEach(goal => {
-      if(!goal.stats){
-        goal.stats = {
-          date: selectedDate,
-          targetPerDuration: goal.defaultTarget,
-          achieved: 0,
-        }
-      }
       achievedTotal += goal.stats.achieved;
       goalTotal += goal.stats.targetPerDuration;
     })
 
-    return ((achievedTotal/goalTotal)*100);
+    return ((achievedTotal / goalTotal) * 100);
   }
 
   return (
     <Grid container justify="center" className={classes.root}>
       <Grid item xs={12} container justify="center">
-        <Button onClick={()=>changeDate(-1)} className={classes.ArrowButton} ><ArrowBack/></Button>
+        <Button onClick={() => changeDate(-1)} className={classes.ArrowButton} ><ArrowBack /></Button>
         <TextField
           id="date"
           label="Date"
@@ -112,15 +117,15 @@ export const Log = () => {
 
           value={selectedDate}
           className={classes.TextField}
-          onChange={(e)=> setSelectedDate(e.target.value)}
+          onChange={(e) => setSelectedDate(e.target.value)}
           InputLabelProps={{
             shrink: true,
           }}
         />
-        <Button onClick={()=>changeDate(1)} className={classes.ArrowButton} ><ArrowForward/></Button>
+        <Button onClick={() => changeDate(1)} className={classes.ArrowButton} ><ArrowForward /></Button>
       </Grid>
       {categories.map((category) => {
-       let categoryPercent = getCategoryProgress(category);
+        let categoryPercent = getCategoryProgress(category);
         return (
           <Paper variant="outlined" className={classes.Paper} key={category}>
             <Grid
@@ -132,7 +137,7 @@ export const Log = () => {
               <Grid item xs={12} className={classes.categoryBackground} >
                 <Typography variant="h6">{category}</Typography>
                 {/* Temporary fix, need to adjust when getCategoryProgress filters a task without history for the date */}
-                <LinearProgress variant="determinate" value={isNaN(categoryPercent)?0:categoryPercent} />
+                <LinearProgress variant="determinate" value={isNaN(categoryPercent) ? 0 : categoryPercent} />
               </Grid>
               {goals.map((goal, index) => (
                 <GoalTracker
