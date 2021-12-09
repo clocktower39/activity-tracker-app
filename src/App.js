d83d3895156c6f0f6e7c3502f8ca093f3ebd87e3
clocktower39 <clocktower39@aol.com>
2021-12-08 20:03:06 -0700

    theme, circular progross background circle, import fonts

--- src/App.js
+++ src/App.js
@@ -1,49 +1,51 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { Container } from '@mui/material';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Container, ThemeProvider } from "@mui/material";
 import { getActivities, loginJWT } from "./Redux/actions";
-import Log from './Components/log/Log';
-import Navbar from './Components/Navbar';
-import Metrics from './Components/Metrics/Metrics';
-import EditLayout from './Components/Edit/EditLayout';
-import AuthRoute from './Components/AuthRoute';
-import Login from './Components/Login';
-import SignUp from './Components/SignUp';
-import './App.css';
+import Log from "./Components/log/Log";
+import Navbar from "./Components/Navbar";
+import Metrics from "./Components/Metrics/Metrics";
+import EditLayout from "./Components/Edit/EditLayout";
+import AuthRoute from "./Components/AuthRoute";
+import Login from "./Components/Login";
+import SignUp from "./Components/SignUp";
+import { theme } from './theme';
+import "./App.css";
 
 function App() {
   const dispatch = useDispatch();
 
   const handleLoginAttempt = async (e) => {
-    dispatch(loginJWT(localStorage.getItem('JWT_AUTH_TOKEN')));
-  }
-  
-  useEffect(()=>{
-    if(localStorage.getItem('JWT_AUTH_TOKEN')!==null){
-        handleLoginAttempt();
+    dispatch(loginJWT(localStorage.getItem("JWT_AUTH_TOKEN")));
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("JWT_AUTH_TOKEN") !== null) {
+      handleLoginAttempt();
     }
     // eslint-disable-next-line
-  },[])
-  useEffect(()=>{
+  }, []);
+  useEffect(() => {
     dispatch(getActivities());
-  // eslint-disable-next-line
-  },[])
-  
+    // eslint-disable-next-line
+  }, []);
 
   return (
-    <Router basename="/activity-tracker/">
-      <Container maxWidth={'md'}>
-        <Switch>
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/signup' component={SignUp} />
-          <AuthRoute exact path='/' component={Log} />
-          <AuthRoute exact path='/edit' component={EditLayout} />
-          <AuthRoute exact path='/metrics' component={Metrics} />
-        </Switch>
-      </Container>
-      <Navbar />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router basename="/activity-tracker/">
+        <Container maxWidth={"md"}>
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={SignUp} />
+            <AuthRoute exact path="/" component={Log} />
+            <AuthRoute exact path="/edit" component={EditLayout} />
+            <AuthRoute exact path="/metrics" component={Metrics} />
+          </Switch>
+        </Container>
+        <Navbar />
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/Components/log/GoalTracker.js
+++ src/Components/log/GoalTracker.js
@@ -1,5 +1,5 @@
 import React from "react";
-import useLongPress from '../../Hooks/useLongPress';
+import useLongPress from "../../Hooks/useLongPress";
 import { useDispatch } from "react-redux";
 import {
   Box,
@@ -7,38 +7,38 @@ import {
   Grid,
   IconButton,
   Typography,
-} from '@mui/material';
-import { makeStyles } from '@mui/styles';
+  circularProgressClasses,
+} from "@mui/material";
+import { makeStyles } from "@mui/styles";
 import { updateActivityProgress } from "../../Redux/actions";
 
 const useStyles = makeStyles({
-  root:{},
-  CircularPercent:{
-    fontSize: '16px'
+  root: {},
+  CircularPercent: {
+    fontSize: "16px",
   },
-  CircularProgressLabel:{
-    fontSize: '13px',
+  CircularProgressLabel: {
+    fontSize: "13px",
   },
-  goalContent:{
-    justifyContent: 'center',
+  goalContent: {
+    justifyContent: "center",
   },
-})
+});
 
 export default function GoalTracker(props) {
   const classes = useStyles();
-  let currentDayStats = props.goal.history.filter(day => day.date === props.selectedDate)[0];
+  let currentDayStats = props.goal.history.filter((day) => day.date === props.selectedDate)[0];
 
   if (currentDayStats === undefined) {
     currentDayStats = {
       date: props.selectedDate,
       targetPerDuration: Number(props.goal.defaultTarget),
       achieved: 0,
-    }
+    };
   }
   let circularProgressPercent = Number(
     (currentDayStats.achieved / currentDayStats.targetPerDuration) * 100
-  )
-
+  );
 
   if (circularProgressPercent > 100) {
     circularProgressPercent = 100;
@@ -52,12 +52,12 @@ export default function GoalTracker(props) {
   };
 
   const onClick = () => {
-    handleActivityUpdate(props.index, 1)
-  }
+    handleActivityUpdate(props.index, 1);
+  };
 
   const onLongPress = () => {
     if (currentDayStats.achieved - 1 >= 0) {
-      handleActivityUpdate(props.index, -1)
+      handleActivityUpdate(props.index, -1);
     }
   };
 
@@ -69,26 +69,41 @@ export default function GoalTracker(props) {
 
   return props.goal.category === props.category ? (
     <Grid container item xs={3}>
-      <Grid container item xs={12} className={classes.goalContent} >
-        <IconButton
-          {...longPressEvent}
-        >
+      <Grid container item xs={12} className={classes.goalContent}>
+        <IconButton {...longPressEvent}>
           <Box position="relative" display="inline-flex" alignItems={"center"}>
             <CircularProgress
               variant="determinate"
-              value={
-                circularProgressPercent === 0 ? -0 : circularProgressPercent
-              }
+              sx={{
+                color: (theme) => theme.palette.grey[theme.palette.mode === "light" ? 200 : 800],
+              }}
+              size={60}
+              thickness={5}
+              value={100}
+            />
+            <CircularProgress
+              variant="determinate"
+              disableShrink
+              value={circularProgressPercent === 0 ? -0 : circularProgressPercent}
+              sx={{
+                color: (theme) => (theme.palette.mode === "light" ? "#1a90ff" : "#308fe8"),
+                animationDuration: "550ms",
+                position: "absolute",
+                left: 0,
+                [`& .${circularProgressClasses.circle}`]: {
+                  strokeLinecap: "round",
+                },
+              }}
               size={60}
               thickness={5}
               style={
                 circularProgressPercent <= 0
                   ? { color: "#ccc" }
                   : circularProgressPercent < 34
-                    ? { color: "red" }
-                    : circularProgressPercent < 51
-                    ? { color: "yellow" }
-                    : { color: "green" }
+                  ? { color: "red" }
+                  : circularProgressPercent < 51
+                  ? { color: "yellow" }
+                  : { color: "green" }
               }
             />
             <Box
@@ -101,11 +116,7 @@ export default function GoalTracker(props) {
               alignItems="center"
               justifyContent="center"
             >
-              <Typography
-                variant="h6"
-                component="div"
-                className={classes.CircularPercent}
-              >
+              <Typography variant="h6" component="div" className={classes.CircularPercent}>
                 {`${Math.round(
                   Number((currentDayStats.achieved / currentDayStats.targetPerDuration) * 100)
                 )}%`}
@@ -114,7 +125,7 @@ export default function GoalTracker(props) {
           </Box>
         </IconButton>
       </Grid>
-      <Grid container item xs={12} className={classes.goalContent} >
+      <Grid container item xs={12} className={classes.goalContent}>
         <Typography variant="h6" align="center" className={classes.CircularProgressLabel}>
           {props.goal.task}
         </Typography>
