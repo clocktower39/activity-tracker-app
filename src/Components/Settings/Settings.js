28dc2b4b7c5c9dde5e7e84107a6566a718573274
clocktower39 <clocktower39@aol.com>
2022-06-14 16:20:53 -0700

    OutletList with toggle view

--- src/Components/Settings/Settings.js
+++ src/Components/Settings/Settings.js
@@ -7,6 +7,7 @@ import {
   Button,
   Container,
   Grid,
+  IconButton,
   List,
   ListItem,
   ListItemText,
@@ -15,11 +16,12 @@ import {
   Paper,
   Typography
 } from "@mui/material";
-import { Settings as SettingsIcon } from '@mui/icons-material';
+import { Settings as SettingsIcon, List as ListIcon } from '@mui/icons-material';
 import ChangePassword from './ChangePassword';
 
 export default function Settings() {
   const dispatch = useDispatch();
+  const [openOutletList, setOpenOutletList] = useState(true);
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
 
@@ -35,29 +37,38 @@ export default function Settings() {
     setAnchorEl(null);
   };
 
+  const handleOutletLists = () => setOpenOutletList(prev => !prev);
+
   const handleLogout = () => dispatch(logoutUser());
+
+  const OutletList = () => {
+    return (
+      <List>
+        <ListItem button component={Link} to="/settings">
+          <ListItemText primary="Account" />
+        </ListItem>
+        <ListItem button component={Link} to="/settings/theme">
+          <ListItemText primary="Theme" />
+        </ListItem>
+      </List>
+    )
+  }
 
   return (
     <Container maxWidth="md">
       <Paper sx={{ marginTop: "25px", padding: '12.5px 0', height: '85vh', backgroundColor: 'background.goalContainer', }}>
         <Grid container >
           <Grid container item xs={12} sx={{ justifyContent: 'space-between', padding: '0px 7.5px' }}>
-            <Typography variant="h4" >Settings</Typography>
+            <Box sx={{ display: 'flex', flexDirection: 'row', }}>
+              <IconButton onClick={handleOutletLists} ><ListIcon /></IconButton>
+              <Typography variant="h4" >Settings</Typography>
+            </Box>
             <Button variant="contained" onClick={handleClick}><SettingsIcon /></Button>
           </Grid>
-          <Grid container item xs={12} sm={4} >
-            <Box component={Grid} display={{ xs: "none", sm: "flex" }}>
-              <List>
-                <ListItem button component={Link} to="/settings">
-                  <ListItemText primary="Account" />
-                </ListItem>
-                <ListItem button component={Link} to="/settings/theme">
-                  <ListItemText primary="Theme" />
-                </ListItem>
-              </List>
-            </Box>
+          <Grid container item xs={12} sm={4} sx={{ display: openOutletList ? 'flex' : 'none', }}>
+            <OutletList />
           </Grid>
-          <Grid container item xs={12} sm={8}>
+          <Grid container item xs={12} sm={openOutletList ? 8 : 12}>
             <Outlet />
           </Grid>
         </Grid>
