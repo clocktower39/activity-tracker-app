23216b4f23841c94e5cc6d3cf92b908631f7603a
clocktower39 <clocktower39@aol.com>
2024-12-13 14:43:09 -0700

    vite PWA manifest update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Activity Tracker",
+        short_name: "Tracker",
+        description: "Track your activities effortlessly!",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        scope: "/activity-tracker/",
+        start_url: "/activity-tracker/",
+        icons: [
+          {
+            src: "/activity-tracker/assets/favicon.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/activity-tracker/assets/favicon.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    }),
+  ],
   base: "/activity-tracker/",
 })
